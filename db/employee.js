82fd52e637aab9e17461e0a4855ddb820b2a5082
Molly Wilson <molly_wilson1@outlook.com>
2021-06-10 11:34:15 -0500

    add an employee to the database

--- db/employee.js
+++ db/employee.js
@@ -11,4 +11,18 @@ const getEmployees = () => {
         return rows;
     });
 };
-module.exports = {getEmployees};
+
+const addEmployee = newEmployee => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`
+    const params = [newEmployee.first_name, newEmployee.last_name, newEmployee.role_id, newEmployee.manager_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        return result;
+    });
+};
+
+module.exports = { getEmployees, addEmployee };
